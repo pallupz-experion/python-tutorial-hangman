d0d4f5288160ae78a90eabb952a262e0b4a9266a
Antony Pallupetta <antony.pallupetta@experionglobal.com>
2022-01-19 13:07:46 +0530

    commented and rearranged

--- hangman.py
+++ hangman.py
@@ -3,51 +3,6 @@ from utils import get_random_word, get_unmasked_word, get_valid_guess
 
 
 def start_game():
-    while True:
-        guessed_letters = []
-        incorrect_guess_count = 6
-        
-        word = get_random_word()
-        # word = 'blah'
-        print(f"Okay, here we go: The secret word is {len(word)} characters long.\n")
-        masked_word = '_' * len(word)
-        
-        while True:
-            print("\n")
-            print(f"Secret Word: {masked_word}")
-
-            if masked_word == word:
-                print(f"Congrats! You correctly guessed the word: {word}. GAME OVER!")
-                break
-
-            if incorrect_guess_count == 0:
-                print(f"Uh oh!! GAME OVER! You have run out of guesses. The word is: {word}")
-                break
-            
-            print(f"So far you have guessed: {sorted(guessed_letters) if guessed_letters else 'Nothing'}")
-            guess = get_valid_guess(guessed_letters)
-            guessed_letters.append(guess)
-
-            if guess in word:
-                print(f"\nYay! That was a good guess! The letter '{guess}' exists in the secret word.")
-
-            else: 
-                print(f"\nUh oh! That was NOT a great guess! The letter '{guess}' does NOT exist in the secret word.")
-                incorrect_guess_count -= 1
-                print(f"You have {incorrect_guess_count} incorrect guesses left.")
-
-            masked_word = get_unmasked_word(word, masked_word, guess)
-
-        play = input("\nWould you like to play again? (y/n): ")
-        if play.lower() != 'y':
-            print("Bye Bye! Hope you had fun!")
-            break
-        
-        else:
-            print("----------------------------------------------------------------------------------------------------\n")
-
-
-if __name__ == '__main__':
     print("\n------------------------------------- Welcome to Hangman CLI game! ------------------------------------- ")
     
     intro = """
@@ -60,4 +15,58 @@ if __name__ == '__main__':
     """
     print(dedent(intro))
     
+    # Loop to play multiple times
+    while True:
+
+        # Initialize letters list and count 
+        guessed_letters = []
+        incorrect_guess_count = 6
+        
+        # get random word
+        word = get_random_word()
+
+        # create masked word
+        print(f"Okay, here we go: The secret word is {len(word)} characters long.\n")
+        masked_word = '_' * len(word)
+        
+        while True:
+            print("\n")
+            print(f"Secret Word: {masked_word}")
+
+            # Check for Game Over criteria and break loop
+            if masked_word == word:
+                print(f"Congrats! You correctly guessed the word: {word}. GAME OVER!")
+                break
+
+            if incorrect_guess_count == 0:
+                print(f"Uh oh!! GAME OVER! You have run out of guesses. The word is: {word}")
+                break
+            
+            # Get a valid guess
+            print(f"So far you have guessed: {sorted(guessed_letters) if guessed_letters else 'Nothing'}")
+            guess = get_valid_guess(guessed_letters)
+            guessed_letters.append(guess)
+
+            # Check if guess was good
+            if guess in word:
+                print(f"\nYay! That was a good guess! The letter '{guess}' exists in the secret word.")
+
+            else: 
+                print(f"\nUh oh! That was NOT a great guess! The letter '{guess}' does NOT exist in the secret word.")
+                incorrect_guess_count -= 1
+                print(f"You have {incorrect_guess_count} incorrect guesses left.")
+
+            # unmask the word using guessed letter 
+            masked_word = get_unmasked_word(word, masked_word, guess)
+
+        play = input("\nWould you like to play again? (y/n): ")
+        if play.lower() != 'y':
+            print("Bye Bye! Hope you had fun!")
+            break
+        
+        else:
+            print("----------------------------------------------------------------------------------------------------\n")
+
+
+if __name__ == '__main__':    
     start_game()
--- utils.py
+++ utils.py
@@ -12,6 +12,10 @@ def get_random_word():
 
 
 def get_unmasked_word(word, masked_word, guess):
+    '''
+    This function takes the parameters "secret word", "masked word" as it is present now, and letter to use for unmasking.
+    If the letter is present in the word, the function would return a new masked word with only that letter unmasked at all applicable positions.
+    '''
     masked_word = list(masked_word)
     if guess in word:
         positions = [pos for pos, char in enumerate(word) if char == guess]
@@ -22,6 +26,9 @@ def get_unmasked_word(word, masked_word, guess):
 
 
 def get_valid_guess(guessed_letters):
+    '''
+    This function returns a valid user input. User input is valid if it does not already exist in the input list and is an alphabet.
+    '''
     guess = input("Enter your guess: ")
     
     if len(guess) != 1:
